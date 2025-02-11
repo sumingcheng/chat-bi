0dbaf2ad6000959190c219b0bb7223411eb18061
sumingcheng <cjwdyzdyplxjg@gmail.com>
2025-02-11 17:12:09 +0800

    增加必要的数据，修改的文件

--- app/database/drop_table.sql
+++
@@ -1,9 +0,0 @@
-use chat_bi;
-
--- 如果存在则删除已有的表（注意：在生产环境中要谨慎执行此操作）
-drop table if exists sales;
-drop table if exists order_item;
-drop table if exists sales_order;
-drop table if exists customer;
-drop table if exists product;
-drop table if exists category;
