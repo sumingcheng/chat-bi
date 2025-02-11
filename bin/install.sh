dec006f26e17179f141604b314993f5b373e4dde
sumingcheng <cjwdyzdyplxjg@gmail.com>
2025-02-11 21:45:56 +0800

    增加windows环境启动

--- bin/install.sh
+++ bin/install.sh
@@ -3,3 +3,7 @@
 #export HTTPS_PROXY="http://172.22.220.64:7890"
 pip install torch==1.11.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
+pip install -r requirements-win.txt
+
+# 安装 milvus-lite
+pip install milvus-lite==2.4.10
