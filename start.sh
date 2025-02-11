195f9cee0d23295b2ebc32b36401fc768e80d668
sumingcheng <cjwdyzdyplxjg@gmail.com>
2025-02-11 21:45:56 +0800

    增加windows环境启动

--- start.sh
+++ start.sh
@@ -6,18 +6,20 @@ echo "Script directory: $SHELL_FOLDER"
 
 echo "Current PYTHONPATH: $PYTHONPATH"
 
-# 设置 Python 环境变量
-export PYTHONUNBUFFERED=1
+# 设置 Python 日志不缓冲
+export PYTHONUNBUFFERED=1 
+
+# 设置 Python 路径
 export PYTHONPATH=$PYTHONPATH:$SHELL_FOLDER
 
-# 设置代理，如果有使用的话
-export HTTP_PROXY="http://172.22.220.64:7890"
-export HTTPS_PROXY="http://172.22.220.64:7890"
+# 设置代理
+export HTTP_PROXY="http://127.0.0.1:7890"
+export HTTPS_PROXY="http://127.0.0.1:7890"
 
 # 设置数据库和OpenAI API的环境变量
-export OPENAI_API_KEY="xxx"
-export DB_HOST="172.22.220.64"
-export DB_PORT=3306
+export OPENAI_API_KEY="sk-"
+export DB_HOST="127.0.0.1"
+export DB_PORT=13306
 export DB_USER="root"
 export DB_PASSWORD="admin123456"
 export DB_NAME="chat_bi"
