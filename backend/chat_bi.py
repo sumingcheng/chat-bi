476b8e208d3179ef196d38bf036385f6e6a91551
sumingcheng <cjwdyzdyplxjg@gmail.com>
2025-02-25 21:24:57 +0800

    fix: 简化

--- backend/chat_bi.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from backend.middleware.cors import mw_cors
-from backend.routers.api import router
-
-app = FastAPI()
-mw_cors(app)
-
-app.include_router(router)
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
-from backend.chat_bi import app
+from fastapi import FastAPI
+from backend.middleware.cors import mw_cors
+from backend.routers.api import router
 import uvicorn
+
+# 创建FastAPI应用
+app = FastAPI()
+# 应用CORS中间件
+mw_cors(app)
+# 包含API路由
+app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=13000)
