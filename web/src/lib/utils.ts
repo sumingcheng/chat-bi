caf6683dce06631f831028d13714d1053f9629f4
sumingcheng <cjwdyzdyplxjg@gmail.com>
2025-06-15 20:12:55 +0800

    chore: 更新.gitignore并添加工具函数utils.ts

---
+++ web/src/lib/utils.ts
@@ -0,0 +1,16 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: Date | string) {
+  return new Intl.DateTimeFormat('zh-CN', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(new Date(date))
+} 
